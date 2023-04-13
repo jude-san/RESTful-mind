41c33285009637bf3ab8cfb8cc3ad5ad283bf030
jude-san <judekhay@gmail.com>
2023-04-13 15:47:40 +0000

    js file hard break

---
+++ resources/js/components/Main.jsx
@@ -0,0 +1,19 @@
+import React, { Component, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends Component {
+    render() {
+        return(
+            <div>
+                <h3>All Products</h3>
+            </div>
+        );
+    }
+}
+
+export default Main;
+
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<Main />, document.getElementById('root'));
+}
